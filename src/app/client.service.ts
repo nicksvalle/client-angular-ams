77a83f142ca1d93209e691c9b7d7681a7ca72040
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2023-05-02 16:54:43 -0300

    final

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Client } from './client';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getClients(): Observable<Client[]> {
+    let url = 'http://localhost:3000/clients';
+    return this.http.get<Client[]>(url);
+  }
+
+
 }
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Client } from '../client';
+import { ClientService } from './../client.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
+
+  clients : Client[] = [];
+
+  constructor (private ClientService: ClientService){}
+
+  ngOnInit(): void {
+    this.loadClients();
+  }
+
+  loadClients() {
+    this.ClientService.getClients().subscribe(
+      {
+          next : data => this.clients = data
+      }
+    );
+  }
 
 }
